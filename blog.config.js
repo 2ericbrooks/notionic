347ca074c3cacba83b40f93c7c817d58563ab46a
2ericbrooks <132639995+2ericbrooks@users.noreply.github.com>
2024-02-26 17:21:02 -0600

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -1,18 +1,18 @@
 const BLOG = {
-  title: '4eb',
-  author: 'Eric B',
-  email: 'amouage@proton.me',
-  link: 'https://4eb.vercel.app',
-  newsletter: 'Insights',
-  description: 'My blog',
+  title: 'Notionic',
+  author: '左蓝',
+  email: 'i@zuolan.me',
+  link: 'https://zuolan.me',
+  newsletter: 'Notionic Weekly',
+  description: 'A static blog build on top of Notion and Next.js',
   lang: 'en-US', // ['en-US', 'zh-CN', 'zh-HK', 'zh-TW', 'ja-JP', 'es-ES']
-  timezone: 'America/Chicago', // See https://en.wikipedia.org/wiki/List_of_tz_database_time_zones for all options.
+  timezone: 'Asia/Shanghai', // See https://en.wikipedia.org/wiki/List_of_tz_database_time_zones for all options.
   appearance: 'auto', // ['light', 'dark', 'auto'],
   font: 'sans-serif', // ['sans-serif', 'serif']
   lightBackground: '#F6F8FA', // use hex value, don't forget '#' e.g #fffefc
   darkBackground: '#212936', // use hex value, don't forget '#'
   path: '', // leave this empty unless you want to deploy Notionic in a folder
-  since: 2024, // If leave this empty, current year will be used.
+  since: 2022, // If leave this empty, current year will be used.
   postsPerPage: 10,
   sortByDate: true,
   pagesShow: {
@@ -20,32 +20,32 @@ const BLOG = {
     notes: true,
     projects: true,
     contact: true,
-    books: false,
-    friends: false
+    books: true,
+    friends: true
   },
-  showWeChatPay: false,
+  showWeChatPay: true,
   previewImagesEnabled: true,
   autoCollapsedNavBar: false, // The automatically collapsed navigation bar
   ogImageGenerateHost: 'og-zl.vercel.app', // The link to generate OG image, don't end with a slash
   defaultCover: '/cover.jpg',
   socialLink: {
-    twitter: 'https://twitter.com/ovrnightr',
-    github: 'https://github.com/2ericbrooks',
-    telegram: 'https://t.me/ovrnightr'
+    twitter: 'https://twitter.com/izuolan',
+    github: 'https://github.com/izuolan',
+    telegram: 'https://t.me/zuolan'
   },
   seo: {
-    keywords: ['Notionic', 'Eric B', 'Blog'],
+    keywords: ['Notionic', 'Zuolan', 'Blog'],
     googleSiteVerification: '' // Remove the value or replace it with your own google site verification code
   },
   notionPageId: process.env.NOTION_PAGE_ID, // DO NOT CHANGE THIS! Edit .env file!
   notionSpacesId: process.env.NOTION_SPACES_ID, // DO NOT CHANGE THIS! Edit .env file!
   notionAccessToken: process.env.NOTION_ACCESS_TOKEN, // Useful if you prefer not to make your database public
-  notionDomain: '2eb.notion.site',
+  notionDomain: 'izuolan.notion.site',
   telegramToken: process.env.TELEGRAM_TOKEN, // The token of your Telegram bot
   telegramChatId: '263895784', // The chat id of your Telegram bot
   telegramChannelUrl: 'https://channel.zuolan.me/', // The link of your Telegram channel
-  telegramChannelName: 'ericb_me', // The name of your Telegram channel
-  craftConfigShareUrl: 'https://4eb.craft.me/F28lVUe2DIBSjo', // The link to share your craft config
+  telegramChannelName: 'zuolan_me', // The name of your Telegram channel
+  craftConfigShareUrl: 'https://www.craft.do/s/kQtcWqkv98cHhB', // The link to share your craft config
   analytics: {
     provider: '', // Currently we support Google Analytics, Ackee, Umami and Cloudflare Insights, please fill with 'ga' or 'ackee' or 'umami' or 'cf', leave it empty to disable it.
     ackeeConfig: {
